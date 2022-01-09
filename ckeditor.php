eb79bdde8d2b30eecba17476806fb13530ed9817
Nihir <zalanihir8866@gmail.com>
2022-01-08 17:22:54 -0930

    CKEDitor

---
+++ ckeditor.php
@@ -0,0 +1,23 @@
+<script src="https://cdn.ckeditor.com/4.8.0/full-all/ckeditor.js"></script>
+<script type="text/javascript">
+  CKEDITOR.replace('editor', {
+  skin: 'moono',
+  enterMode: CKEDITOR.ENTER_BR,
+  shiftEnterMode:CKEDITOR.ENTER_P,
+  toolbar: [{ name: 'basicstyles', groups: [ 'basicstyles' ], items: [ 'Bold', 'Italic', 'Underline', "-", 'TextColor', 'BGColor' ] },
+             { name: 'styles', items: [ 'Format', 'Font', 'FontSize' ] },
+             { name: 'scripts', items: [ 'Subscript', 'Superscript' ] },
+             { name: 'justify', groups: [ 'blocks', 'align' ], items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+             { name: 'paragraph', groups: [ 'list', 'indent' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent'] },
+             { name: 'links', items: [ 'Link', 'Unlink' ] },
+             { name: 'insert', items: [ 'Image'] },
+             { name: 'spell', items: [ 'jQuerySpellChecker' ] },
+             { name: 'table', items: [ 'Table' ] }
+             ],
+});
+</script>
+<script type="text/javascript">
+    $(document).ready(function() {
+    $('#example').DataTable();
+} );
+</script>
